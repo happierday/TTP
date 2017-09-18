946ee8b43603c5fd72393abc1a927db0b886648b
jiancheng wu <jianchengw26@gmail.com>
2017-09-18 16:03:48 -0400

    Q3

---
+++ Q3.java
@@ -0,0 +1,34 @@
+//*******************************************************************
+// Dear CompileJava users,
+//
+// CompileJava has been operating since 2013 completely free. If you
+// find this site useful, or would otherwise like to contribute, then
+// please consider a donation (link in 'More Info' tab) to support
+// development of the new CompileJava website (stay tuned!).
+//
+// Most sincerely, Z.
+//*******************************************************************
+import java.util.Stack;
+public class Q3 {
+    public static void main(String args[]) {
+        String s = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.";
+        System.out.println(stringReformatting(s,0));
+    }
+    
+    public static int stringReformatting(String s, int k){
+      	if(s == null || s.length() == 0 || k < 0 || k > s.length() || s.charAt(k) != '(') return -1;
+      	Stack<Character> stack = new Stack<Character>();
+      	stack.push(s.charAt(10));
+      	int result= 0;
+      	for(int i = k+1 ; i<s.length();i++){
+      		if(s.charAt(i) == '(') stack.push(s.charAt(i));
+          	else if(s.charAt(i) == ')'){
+            		stack.pop();
+              		if(stack.isEmpty()){
+                        return i;
+                    }
+            }
+        }
+      	return 0;
+    }
+}
